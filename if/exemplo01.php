1f34c1c3fc854794f53d9f1de7633edf07178d97
Luciano Carvalho <lucianofdc@outlook.com>
2022-07-13 00:25:14 -0300

    if

---
+++ if/exemplo01.php
@@ -0,0 +1,25 @@
+<?php
+
+ $qualSuaIdade = 11;
+
+ $idadeCrianca = 12;
+ $idadeMaior   = 18;
+ $idadeMelhor  = 65;
+
+
+if( $qualSuaIdade < $idadeCrianca ){
+   echo "Criança";
+}else if( $qualSuaIdade < $idadeMaior ){
+   echo "Adolecente";
+}else if( $qualSuaIdade < $idadeMelhor ){
+   echo "Adulto";
+}else{
+   echo "Idoso";
+}
+
+echo "<br>";
+
+echo ( $qualSuaIdade < $idadeMaior)?"Menor de Idade":"Mairo de Idade";
+
+
+?>
